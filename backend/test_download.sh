304562c6c3a7e3cfe5146cb21e564a2a5562e434
João Lourenço <jmguellourenco@gmail.com>
2025-07-08 14:41:09 +0100

    removed local test files from git

--- backend/test_download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -X GET http://localhost:8000/api/download/output.zip --output output.zip
--- backend/tests/main.js
+++
@@ -1,7 +0,0 @@
-import Query from "@specs-feup/lara/api/weaver/Query.js";
-import { FunctionJp } from "@specs-feup/clava/api/Joinpoints.js";
-for (const $function of Query.search(FunctionJp)) {
-    console.log($function.name);
-}
-console.log("Done");
-//# sourceMappingURL=main.js.map
