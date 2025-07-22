8a80ce1415c5dac16d42989eefd08903f6f6f67c
Isaac3105 <isaac.absantos@gmail.com>
2025-07-22 13:03:15 +0100

    Thw weaver extension is completely adapted to the new and improved configjson

--- frontend/extensions/weaver/src/htmlTemplate.ts
+++ frontend/extensions/weaver/src/htmlTemplate.ts
@@ -4,7 +4,39 @@ import { ScriptProvider } from './scripts';
 import path from 'path';
 import * as fs from 'fs';
 
+interface Option{
+    name: string,
+    values?: string[],
+    type: string
+}
+
+interface Logo {
+    link: string,   
+    fileName: string,
+    label: string,
+    width: number,
+    height: number
+}
+
 export class HtmlTemplateProvider {
+
+    static createLogoImage(webview: vscode.Webview, extensionUri: vscode.Uri, logos : Logo[]) : string{
+        let res = ``;
+        for(const logo of logos){
+            res += `
+            <a href="${logo.link}" target="_blank">
+                <img src=${webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', `${logo.fileName}`))} alt = "${logo.label}" width=${logo.width} height=${logo.height}>
+            </a>
+            `;
+        }
+        return res;
+    }
+
+    static createImage(tool : string, extensionUri: vscode.Uri, webview : vscode.Webview, imgWidth: number, imgHeight: number ){
+        return `
+        <img src=${webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', `${tool}.png`))} alt="${tool}" width=${imgWidth} height=${imgHeight}>
+        `;
+    }
     static button(name : string, backendUrl: string): string {
         const button = `
         <button id="${name}" class ="weaver-button" onclick="onButtonClick()"><span class="text">Weave Application</span></button>
@@ -15,9 +47,9 @@ export class HtmlTemplateProvider {
         return button;
     }
 
-    static select(name : string, tool : string, values: string[]): string {
+    static select(name : string, values: string[]): string {
         const select = `
-        <div id = "${name}" class="custom-select";">
+        <div id = "${name}" class="custom-select" style = "display:flex; flex-direction: column" >
             <p>Please select a standard:</p>
             <select id="standard-select" onchange="onDropdownChange()">
             </select>
@@ -28,21 +60,20 @@ export class HtmlTemplateProvider {
         return select;
     }
 
-    static assembleOptions(config : any , tool: string, backendUrl: string, extensionUri: vscode.Uri): string {
-        const toolConfig = config[tool ?? "clava"];
-        if (!toolConfig?.options) {
-            console.error(`No options found for tool: ${tool}`);
-            return '';
-        }
+    static assembleOptions(toolOptions : Option[], tool : string, backendUrl: string): string {
         let res = '';
-        for(const option of toolConfig["options"]){
+        for(const option of toolOptions){
             switch (option.type) {
                 case 'button':
                     res += this.button(option.name, backendUrl);
                     break;
             
                 case 'select':
-                    res += this.select(option.name, tool??"clava", option.values);
+                    if (!option.values) {
+                        console.error(`No options found for tool: ${tool}`);
+                        break;
+                    }
+                    res += this.select(option.name, option.values);
                     break;
             }
         }
@@ -59,8 +90,8 @@ export class HtmlTemplateProvider {
         let config;
         let img_width;
         let img_height;
-        const img_disk = vscode.Uri.joinPath(extensionUri, 'media', `${tool}.png`);
-        const img_path = webview.asWebviewUri(img_disk);
+        let logos;
+        let toolOptions;
         
         try {
             const workspaceFolder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
@@ -79,6 +110,16 @@ export class HtmlTemplateProvider {
             
             const raw = fs.readFileSync(configPath, 'utf-8');
             config = JSON.parse(raw);
+
+            const toolConfig = config[tool ?? "clava"];
+            if (!toolConfig?.options) {
+                console.error(`No options found for tool: ${tool}`);
+                return '';
+            }
+
+            toolOptions = toolConfig["options"];
+
+            logos = config[tool?? "clava"]["logos"];
             
             img_width = config[tool?? "clava"]["image"]["img_width"];
             img_height = config[tool?? "clava"]["image"]["img_height"];
@@ -93,36 +134,28 @@ export class HtmlTemplateProvider {
         <html lang="en">
             <body>
                 <div style="display: flex; flex-direction: row;">
-                    <div style="height:100vh; max-width:fit-content; display:flex; flex-direction: column; gap: 30px; padding: 10px">
+                    <div style="height:100vh; max-width:fit-content; display:flex; flex-direction: column; gap: 30px; align-items: center; padding: 10px">
 
-                        <div style="display:flex; flex-direction: row; justify-content: center">
-                            <img src=${img_path} alt="${tool}" width=${img_width} height=${img_height}>
-                        </div>
+                        ${this.createImage(tool,extensionUri,webview,img_width,img_height)}
 
                         <style>
                             .profiles-editor .sidebar-view {
                                 height:100%;
                                 width: 320px;
                             }
-                            
                             ${StyleProvider.getDropDownStyle()}
                             ${StyleProvider.getWeaveButtonStyle(tool)}
                             ${StyleProvider.getLoaderStyle(tool)}
                         </style>
 
-                        <div style="display: flex; flex-direction: column; gap: 20px; align-items: center">
+                        <div style="display:flex; flex-direction: column; gap: 20px">
 
-                            ${this.assembleOptions(config, tool, backendUrl, extensionUri)}
+                            ${this.assembleOptions(toolOptions, tool, backendUrl)}
 
                         </div>
 
-                        <div style="display: flex; flex-direction: column; gap: 10px; align-items: center; margin-top: auto; margin-bottom: 10px">
-                            <a href="https://specs.fe.up.pt/" target="_blank">
-                                <img src=${webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', `specs_logo.png`))} alt="SPECS-logo" width=170 height=67>
-                            </a>
-                            <a href="https://sigarra.up.pt/feup" target="_blank">
-                                <img src=${webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'media', `feup_logo.png`))} alt="FEUP-logo" width=170 height=59>
-                            </a>
+                        <div style="display:flex; flex-direction: column; gap: 10px; margin-top: auto; margin-bottom: 10px">
+                            ${this.createLogoImage(webview,extensionUri,logos)}
                         </div>
                     </div>
                 </div>
