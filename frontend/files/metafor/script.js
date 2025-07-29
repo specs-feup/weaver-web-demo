1df1d6e0c14bdc613306dae0b501094105e19ef2
Isaac3105 <isaac.absantos@gmail.com>
2025-07-29 14:38:41 +0100

    Started on the metafor tool

---
+++ frontend/files/metafor/script.js
@@ -0,0 +1,5 @@
+import Query from "@specs-feup/lara/api/weaver/Query.js";
+
+
+
+console.log(Query.root().code);
---
+++ run_metafor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+trap_ctrl_c() {
+    echo "Caught CTRL+C. Shutting down..."
+    docker compose down
+    clear
+    exit
+}
+
+trap "trap_ctrl_c" 2
+
+export TOOL=metafor
+
+cd frontend/
+sh package_extensions.sh
+cd ..
+
+docker compose up --build &
+compose_pid=$!
+
+# Wait for docker compose to finish, trap will work while waiting
+wait $compose_pid
+#trap_ctrl_c
